fe5be751300d8d889f0ecb9fa19b3d8b58ad1b26
Dmytro Prokopovych <dmytro.prokopovych@vgs.io>
2019-11-01 14:07:33 +0200

    fix for dump and test

--- integrations/visa/v1/test.js
+++ integrations/visa/v1/test.js
@@ -35,11 +35,43 @@ let card_number_token = '';
 let card_cvv_token = '';
 
 const initialData = {
-  card_name: 'John Doe',
   card: '4111111111111111',
   cvv: '344',
-  card_expirationDate: '01 / 2022',
 }
+
+const params = {
+  "systemsTraceAuditNumber": 451000,
+  "retrievalReferenceNumber": "330000550000",
+  "localTransactionDateTime": "2019-10-31T08:50:30",
+  "acquiringBin": 408999,
+  "acquirerCountryCode": "840",
+  "transactionCurrencyCode": "USD",
+  "senderName": "John Smith",
+  "senderCountryCode": "USA",
+  "senderAddress": "44 Market St.",
+  "senderCity": "San Francisco",
+  "senderStateCode": "CA",
+  "recipientName": "Adam Smith",
+  "recipientPrimaryAccountNumber": "4957030420210462",
+  "amount": "42.00",
+  "businessApplicationId": "AA",
+  "transactionIdentifier": 381228649430011,
+  "merchantCategoryCode": 6012,
+  "sourceOfFundsCode": "03",
+  "cardAcceptor": {
+    "name": "Acceptor 1",
+    "terminalId": "365539",
+    "idCode": "VMT200911026070",
+    "address": {
+      "state": "CA",
+      "county": "081",
+      "country": "USA",
+      "zipCode": "94105"
+    }
+  },
+  "feeProgramIndicator": "123"
+};
+
 
 const check = async () => {
   // should send inbound and get response from echo
@@ -48,31 +80,31 @@ const check = async () => {
     method: 'POST',
     headers: { 'Content-type': 'application/json' },
     body: JSON.stringify(initialData),
-  }).then(res => console.log(res)).then((r) => {
-  // }).then(res => res.json()).then((r) => {
+  }).then(res => res.json()).then((r) => {
     card_number_token = _.get(r, 'json.card');
     card_cvv_token = _.get(r, 'json.cvv');
-    console.log('card number and cvv successfully replaced with tokens');
+    console.log('card number and cvv successfully replaced with alias');
     console.log({
-      ...initialData,
       card_number: card_number_token,
       card_cvv: card_cvv_token,
     })
-
-    const params = {
-      "cardCvv2Value": card_cvv_token,
-      "senderPrimaryAccountNumber": card_number_token
+    const testData = {
+      ...params,
+      cardCvv2Value: card_cvv_token,
+      senderPrimaryAccountNumber: card_number_token,
     };
 
+    const body = JSON.stringify(testData)
+
     // should send outbound and get response from visa
-    console.log('sending redacted test data from proxy to Visa Direct');
+    console.log('sending redacted test data from proxy to Visa Direct', body, testData);
     fetch(outboundUrl, {
+      body,
       method: 'POST',
       headers: {
-        'Content-type': 'application/x-www-form-urlencoded',
+        'Content-type': 'application/json',
         'Authorization': `Basic ${process.env.authToken}`,
       },
-      body: JSON.stringify(params),
       agent: agent,
     }).then((r) => {
       if (r.status === 200) {
--- integrations/visa/v1/visa-direct-outbound.md
+++
@@ -1,5 +0,0 @@
-Reveal the credit card for Visa Direct via a JSON Object containing the `cardCvv2Value` and `senderPrimaryAccountNumber` as keys.
-https://developer.visa.com/capabilities/visa_direct/docs
-
-- Reveal the JSON path entries $.cardCvv2Value in the request targets: body
-- Reveal the JSON path entries $.senderPrimaryAccountNumber in the request targets: body
