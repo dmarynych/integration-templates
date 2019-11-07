00978c8e5e8e86aee3ac38be886fd421870236a7
DProkopovych <ms.proks.tst@live.com>
2019-11-07 16:15:54 +0200

    fix for yaml updates + promptly (#10)
    * fix for yaml updates + promptly
    
    * Update integrations/stripe/formData/dump.yaml
    
    Co-Authored-By: Dima Marynych <starkk@ukr.net>

--- src/utils/yaml.js
+++ src/utils/yaml.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const jsyaml = require('js-yaml');
+const promptly = require('promptly');
 const deepDiff = require('deep-diff').diff;
 const vgsCli = require('./vgs-cli');
 const cmn = require('./common');
@@ -14,7 +15,7 @@ const getYamlFile = (requestedIntegration, requestedIntegrationVersion) => {
   }
 }
 
-const parsePerfectCase = (dump, requestedIntegration, requestedIntegrationVersion) => {
+const parsePerfectCase = async (dump, requestedIntegration, requestedIntegrationVersion) => {
   const tplsDumpPath = `integrations/${requestedIntegration}/${requestedIntegrationVersion}/dump.yaml`;
 
   const tplDump = jsyaml.safeLoad(
@@ -26,7 +27,7 @@ const parsePerfectCase = (dump, requestedIntegration, requestedIntegrationVersio
     console.log('Yaml files are same');
     return;
   } else {
-    const parsedDump = parseYamlDump(dump, tplDump);
+    const parsedDump = await parseYamlDump(dump, tplDump);
     if (!parsedDump) {
       console.log('parsed dump not valid, aborting');
       return;
@@ -41,10 +42,11 @@ const parsePerfectCase = (dump, requestedIntegration, requestedIntegrationVersio
   }
 }
 
-const parseYamlDump = (dump, tplDump) => {
+const parseYamlDump = async (dump, tplDump) => {
   // TODO add possibility to choose routes to sync by id
   const result = {
     inbound: false,
+    dumpInbound: false,
     outbound: {
       // id: route
     },
@@ -69,28 +71,11 @@ const parseYamlDump = (dump, tplDump) => {
   dump.data.forEach(route => {
     isRoutesExists = true;
     if (cmn.getProxyType(route.attributes) === 'inbound') {
-      if (result.inbound && result.inbound.id === route.id) {
-        result.inbound = route; // TODO show diff & ask to rewrite
-      } else {
-        // route id same as tpl, just rewrite
-        result.inbound = {
-          ...route,
-          id: result.inbound.id,
-          attributes: {
-            ...route.attributes,
-            id: result.inbound.id,
-          }
-        };
-      }
-    } else {
-      if (result.outbound[route.id]) {
-        result.outbound[route.id] = route; // route id same as tpl, just rewrite
-      } else {
-        // TODO show diff & ask to rewrite
-        result.outbound[route.id] = route;
-      }
+      result.dumpInbound = route;
+    } else if (!result.outbound[route.id]) {
+      result.outbound[route.id] = route;
     }
-  })
+  });
 
   // convert result -> array -> yaml dump
   result.inbound && alt_result.data.push(result.inbound);
@@ -98,7 +83,25 @@ const parseYamlDump = (dump, tplDump) => {
     if (result.outbound.hasOwnProperty(route)) alt_result.data.push(result.outbound[route]);
   }
 
+  if (result.dumpInbound || result.inbound) {
+    alt_result.data.push(await inboundWorker(result.dumpInbound, result.inbound));
+  }
+
   return jsyaml.safeDump(alt_result);
+}
+
+const inboundWorker = async (dumpRoute, tplInbound) => {
+  if (tplInbound && tplInbound.id !== dumpRoute.id) {
+    // route id differs from tpl id, show diff & ask rewrite
+    const shouldRewrite = await promptly.confirm('You already have inbound route, rewrite?:');
+    if (shouldRewrite) {
+      tplInbound.id = dumpRoute.id;
+      tplInbound.attributes.id = dumpRoute.id;
+      return tplInbound;
+    }
+  } else {
+    return dumpRoute;
+  }
 }
 
 module.exports = {
