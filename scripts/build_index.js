3f0d92f8557288329e18efee432596b2798b1c78
Dima Marynych <dmarynych@gmail.com>
2019-12-02 13:34:37 +0200

    wip

--- scripts/build_index.js
+++ scripts/build_index.js
@@ -3,56 +3,71 @@ const jsyaml = require('js-yaml');
 var ncp = require('ncp').ncp;
 
 const integrationsPath = './integrations';
-const dirs = fs.readdirSync(integrationsPath);
-
-const integrations = [];
-
-dirs.forEach(dir => {
-  const intDir = `${integrationsPath}/${dir}`;  
-  const versionDir = fs.readdirSync(intDir).find(p => fs.lstatSync(`${intDir}/${p}`).isDirectory());
-  const logoFile = fs.readdirSync(intDir).find(p => p.includes('logo'));
-
-  const dump = fs.readFileSync(`${intDir}/${versionDir}/dump.yaml`).toString();
-
-  integrations.push({
-    logoFile,
-    name: dir,
-    config: 'config.json',
-    readme: 'README.md',
-    versions: [
-      {
-        version: versionDir,
-        files: fs.readdirSync(`${intDir}/${versionDir}`)
-      }
-    ]
-  });
-  // const routes = jsyaml.safeLoad(dump);
-  // routes.data.forEach(route => {
-  //   if(route.attributes.destination_override_endpoint === '*') {
-  //     if(!route.attributes.tags.integration) {
-  //       throw new Error(`${dir} integration should have tags.integration field`);
-  //     }
-  //   }
-
-  //   if(!route.attributes.tags.name) {
-  //     throw new Error(`${dir}/${versionDir} integration should have tags.name field`);
-  //   }
-  // });
-
-  
-});
-
 const distDir = './dist';
-if(!fs.existsSync(distDir)){
-  fs.mkdirSync(distDir);
+
+run();
+
+async function run() {
+  await copyIntegrations(distDir);
+  const index = buildIndex(`${distDir}/${integrationsPath}`);
+  fs.writeFileSync(`${toDir}/index.json`, JSON.stringify(index));  
 }
 
-ncp(integrationsPath, distDir, function (err) {
-  if (err) {
-    console.error(err);
-  }
+async function buildIndex(path) {
+  const integrations = [];
+  const dirs = fs.readdirSync(path);
+  dirs.forEach(dir => {
+    const intDir = `${path}/${dir}`;  
+    const versionDir = fs.readdirSync(intDir).find(p => fs.lstatSync(`${intDir}/${p}`).isDirectory());
+    const logoFile = fs.readdirSync(intDir).find(p => p.includes('logo'));
 
-  fs.writeFileSync(`${distDir}/index.json`, JSON.stringify(integrations));
-});
+    const dump = fs.readFileSync(`${intDir}/${versionDir}/dump.yaml`).toString();
 
+    integrations.push({
+      logoFile,
+      name: dir,
+      config: 'config.json',
+      readme: 'README.md',
+      versions: [
+        {
+          version: versionDir,
+          files: fs.readdirSync(`${intDir}/${versionDir}`)
+        }
+      ]
+    });
+
+    return integrations;
+    // const routes = jsyaml.safeLoad(dump);
+    // routes.data.forEach(route => {
+    //   if(route.attributes.destination_override_endpoint === '*') {
+    //     if(!route.attributes.tags.integration) {
+    //       throw new Error(`${dir} integration should have tags.integration field`);
+    //     }
+    //   }
+
+    //   if(!route.attributes.tags.name) {
+    //     throw new Error(`${dir}/${versionDir} integration should have tags.name field`);
+    //   }
+    // });
+
+    
+  }); 
+}
+
+async function copyIntegrations(toDir) {
+  return new Promise((resolve, rejeect) => {
+    if(!fs.existsSync(toDir)){
+      fs.mkdirSync(toDir);
+    }
+    
+    ncp(integrationsPath, toDir, function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+
+}
 console.log(JSON.stringify(integrations, null, 2));
